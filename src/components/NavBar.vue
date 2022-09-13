dc06a8e55f18d5f266fa04e8f0d527d1fda34feb
Ember Reyes <ereyes42@stu.southtexascollege.edu>
2022-09-13 17:07:14 -0500

    added navbar layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref('🏦Company Directory')
+</script>
+
+<template>
+  <nav>
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" class="menu-item">Department</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-login">Logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-16 items-center justify-between bg-teal-500 text-emerald-900;
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand {
+        &-title {
+          @apply text-xl font-bold text-sky-900;
+        }
+      }
+      .menu {
+        @apply flex gap-4;
+        &-item {
+          @apply py-1 px-1 font-bold hover:font-extrabold hover:text-emerald-900;
+        }
+        &-login {
+          @apply rounded-md bg-red-300 py-1 px-1 font-bold hover:font-extrabold hover:text-red-700;
+        }
+      }
+    }
+  }
+</style>
