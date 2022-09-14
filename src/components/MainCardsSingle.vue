ea5d549f474d1ab8daf7ee919ea3d00ed32c291d
Ember Reyes <ereyes42@stu.southtexascollege.edu>
2022-09-14 16:40:09 -0500

    added Cards

---
+++ src/components/MainCardsSingle.vue
@@ -0,0 +1,51 @@
+<script setup>
+  import { faker } from '@faker-js/faker'
+
+  const firstName = faker.name.firstName()
+  const lastName = faker.name.lastName()
+  const fullName = `${firstName} ${lastName}`
+
+  const selectCard = () => {
+    console.log(`${fullName} selected`)
+  }
+</script>
+
+<template>
+  <div class="card" @click="selectCard">
+    <div class="card-image">
+      <img :src="faker.internet.avatar()" alt="" srcset="" />
+    </div>
+    <div class="card-details">
+      <p class="card-details-name">{{ fullName }}</p>
+      <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
+      <p class="card-details-email">{{ faker.internet.exampleEmail(firstName, lastName) }}</p>
+      <p class="card-details-quote">{{ faker.lorem.paragraph() }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+  .card {
+    @apply cursor-pointer overflow-hidden rounded-lg bg-emerald-100 p-4 shadow-md transition-transform duration-300 hover:scale-105 hover:shadow-xl hover:shadow-gray-900;
+    &-image {
+      img {
+        @apply mx-auto rounded-full object-contain;
+      }
+    }
+    &-details {
+      @apply flex flex-col gap-2 pt-4 text-center;
+      &-name {
+        @apply text-3xl font-thin  tracking-wide text-sky-900;
+      }
+      &-job {
+        @apply -mt-2 text-xs font-semibold text-lime-600;
+      }
+      &-email {
+        @apply text-sm text-emerald-600;
+      }
+      &-quote {
+        @apply pt-4 text-lg italic text-emerald-800;
+      }
+    }
+  }
+</style>
--- src/components/MainCom.vue
+++ src/components/MainCom.vue
@@ -1,14 +1,15 @@
 <script setup>
   import MainSearch from './MainSearch.vue'
+  import MainCards from './MainCards.vue'
 </script>
 <template>
   <div class="wrapper">
     <MainSearch />
-    <h1>Hello There</h1>
+    <MainCards />
   </div>
 </template>
 <style scoped lang="postcss">
   .wrapper {
-    @apply container mx-auto flex w-96 flex-col gap-1 py-2;
+    @apply container mx-auto flex w-4/5 flex-col gap-1 py-2;
   }
 </style>
